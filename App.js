26f21385e64e3203bcdc9fe71318d29cb8c7fdb4
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-12-19 23:27:25 +0530

    Inital commit of code.

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, WebView } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <WebView style={{ flex: 1 }} source={{ html: 'https://github.com/facebook/react-native' }} />
+        <Text>Vishwesh</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column-reverse',
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
